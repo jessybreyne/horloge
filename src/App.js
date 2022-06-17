fb9d262edf4f16f67ccd2f90158ddc75de9a0d17
Jessy <jessybreyne@protonmail.com>
2022-06-17 15:42:27 +0200

    24h clock

--- src/App.js
+++ src/App.js
@@ -6,25 +6,40 @@ function App() {
   useEffect(() => {
     console.log("Mounted");
     clock();
+    bestClock();
   }, []);
 
   function clock() {
-    const date = new Date();
-    const hours = ((date.getHours() + 11) % 12 + 1);
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const hour = hours * 30;
-    const minute = minutes * 6;
-    const second = seconds * 6;
+    let date = new Date();
+    let hours = ((date.getHours() + 11) % 12 + 1);
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let hour = hours * 30;
+    let minute = minutes * 6;
+    let second = seconds * 6;
 
-    document.querySelector('.heure').style.transform = `rotate(${hour}deg)`;
+    document.getElementById('heure').style.transform = `rotate(${hour}deg)`;
+    document.getElementById('minute').style.transform = `rotate(${minute}deg)`;
+    document.getElementById('seconde').style.transform = `rotate(${second}deg)`;
+  }
 
-    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
+  function bestClock() {
+    let date = new Date();
+    let hours = ((date.getHours() + 23) % 24 + 1);
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    console.log(hours)
+    let bestHour = hours * 15 + 180;
+    let minute = minutes * 6;
+    let second = seconds * 6;
 
-    document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
+    document.getElementById('best-heure').style.transform = `rotate(${bestHour}deg)`;
+    document.getElementById('best-minute').style.transform = `rotate(${minute}deg)`;
+    document.getElementById('best-seconde').style.transform = `rotate(${second}deg)`;
   }
 
   setInterval(clock, 1000);
+  setInterval(bestClock, 1000);
 
   return (
     <div className="App">
@@ -39,9 +54,29 @@ function App() {
       </header>
       <div class="clock">
         <div class="wrap">
-          <span class="heure"></span>
-          <span class="minute"></span>
-          <span class="seconde"></span>
+          <span id="best-heure"></span>
+          <span id="best-minute"></span>
+          <span id="best-seconde"></span>
+          <span class="dot"></span>
+          <div class="sep" id="best-h12">12</div>
+          <div class="sep" id="best-h14">14</div>
+          <div class="sep" id="best-h16">16</div>
+          <div class="sep" id="best-h18">18</div>
+          <div class="sep" id="best-h20">20</div>
+          <div class="sep" id="best-h22">22</div>
+          <div class="sep" id="best-h24">24</div>
+          <div class="sep" id="best-h2">2</div>
+          <div class="sep" id="best-h4">4</div>
+          <div class="sep" id="best-h6">6</div>
+          <div class="sep" id="best-h8">8</div>
+          <div class="sep" id="best-h10">10</div>
+        </div>
+      </div>
+      <div class="clock">
+        <div class="wrap">
+          <span id="heure"></span>
+          <span id="minute"></span>
+          <span id="seconde"></span>
           <span class="dot"></span>
           <div class="sep" id="h0">0</div>
           <div class="sep" id="h1">1</div>
